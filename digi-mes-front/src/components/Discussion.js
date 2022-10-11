342c8dde7e9c8ef7379a58bd4dc05facf79f9118
Jarax <jaraxkapila73@gmail.com>
2022-10-11 17:33:46 +0100

    Get Messages from database

--- digi-mes-front/src/components/Discussion.js
+++ digi-mes-front/src/components/Discussion.js
@@ -1,9 +1,27 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import axios from "axios";
 import { AiOutlineSend } from "react-icons/ai";
+// import { response } from "../../../digi-mes-back/app";
 import { AppContext } from "../App";
 
 export default function Discussion() {
   const { Jakaps } = useContext(AppContext);
+  const [discussion, setDiscussion] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/api/discussions/", {
+        headers: {
+          Accept: "application/json",
+          "Content-type": "application/json",
+        },
+      })
+      .then((response) => setDiscussion(response.data));
+  }, []);
+
+  const sendMessage = () => {};
+
+  console.log(discussion);
   return (
     <section className="discuss">
       <header className="discuss-head">
@@ -17,23 +35,19 @@ export default function Discussion() {
       </header>
       <hr style={{ width: "90%" }} />
       <div className="to-discuss">
-        <p>Salut</p>
-        <p>Salut</p>
-        <p>Salut</p>
-        <p>Salut</p>
-        <p>Salut</p>
-        <p>Salut</p>
-        <p>Salut</p>
-        <p>Salut</p>
-        <p>Salut</p>
-        <p>Salut</p>
-        <p>Salut</p>
-        <p>Salut</p>
-        <p>Salut</p>
+        {!discussion
+          ? []
+          : discussion.map((discuss) => (
+              <div className="discuss-style" key={discuss._id}>
+                <div className="discuss-border">
+                  <p>{discuss.discussion}</p>
+                </div>
+              </div>
+            ))}
       </div>
       <div className="send-message">
         <hr style={{ width: "90%", marginBottom: "20px" }} />
-        <div className="writing-container">
+        <form className="writing-container" onClick={sendMessage}>
           <input
             className="writing"
             type="text"
@@ -42,7 +56,7 @@ export default function Discussion() {
           <div className="send-button">
             <AiOutlineSend style={{ fontSize: "150%", color: "#EAEAEA" }} />
           </div>
-        </div>
+        </form>
       </div>
     </section>
   );
