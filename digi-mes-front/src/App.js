9d2ddc85d1323d6152e991bf91475e4dac181bd3
Jarax <jaraxkapila73@gmail.com>
2022-10-13 17:58:53 +0100

    Create user from the SignUp component

--- digi-mes-front/src/App.js
+++ digi-mes-front/src/App.js
@@ -1,26 +1,34 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Jakaps from "./assets/jakaps.jpg";
 import Home from "./components/Home";
 import SignUp from "./components/SignUp";
-// import Menu from "./components/Menu";
+import Menu from "./components/Menu";
 import Welcome from "./components/Welcome";
 
 export const AppContext = createContext();
 
 function App() {
   const [discussion, setDiscussion] = useState([]);
+  const [token, setToken] = useState('');
+  useEffect(()=>{
+    setToken(token);
+
+  }, [])
   return (
     <AppContext.Provider value={{ Jakaps, discussion, setDiscussion }}>
       <div className="container"> 
-        {/* <Menu /> */}
+        {!token ? <Welcome/> :
+        <>
+        <Menu />
         <Routes>
         
-        <Route path="/signin" element={<Welcome/>} />
         <Route path="/signup" element={<SignUp/>}/>
           <Route exact path="/" element={<Home />} />
           
         </Routes>
+        </>
+        }
       </div>
     </AppContext.Provider>
   );
--- digi-mes-front/src/components/SignUp.js
+++ digi-mes-front/src/components/SignUp.js
@@ -1,15 +1,37 @@
-import React from "react"
+import axios from "axios";
+import React from "react";
 
 export default function SignIn(){
-    return <div className='sign'>
 
-    <div className='welcome'>
-    
-        <form className="signin" >
+      const createUser = (e) =>{
+        e.preventDefault();
+        const user = {
+            firstName: e.target.first.value,
+            lastName: e.target.last.value,
+            email: e.target.mail.value,
+            password: e.target.password.value
+        }
+        
+        axios({
+            method: "POST",
+            url: "http://localhost:5000/api/auth",      
+            data: user,
+        })
+        .then((response) => response)
+        .catch((error) => error);
+        e.target.reset();
+
+    }
+
+    return <div className='sign'>
+    <div className='welcome'>    
+        <form className="signin" 
+        onSubmit={createUser} 
+        >
             <label htmlFor="first">First-Name</label>
             <input type="text" id='first' name='first'/>
-            <label htmlFor="lasst">Last-Name</label>
-            <input type="text" id='first' name='first'/>
+            <label htmlFor="last">Last-Name</label>
+            <input type="text" id='last' name='last'/>
             <label htmlFor="mail">E-mail</label>
             <input type="email" id="mail" name="mail"/>
             <label htmlFor="password">Password</label>
