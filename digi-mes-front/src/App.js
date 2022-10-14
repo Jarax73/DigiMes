d0daff593987fb1de6bfe2d0160db4def10216ad
Jarax <jaraxkapila73@gmail.com>
2022-10-14 20:21:48 +0100

    mistake

--- digi-mes-front/src/App.js
+++ digi-mes-front/src/App.js
@@ -1,5 +1,5 @@
 import React, { createContext, useEffect, useState } from "react";
-import { Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Jakaps from "./assets/jakaps.jpg";
 import Home from "./components/Home";
 import SignUp from "./components/SignUp";
@@ -13,49 +13,51 @@ function App() {
   const [user, setUser] = useState([]);
   const [discussion, setDiscussion] = useState([]);
   const [token, setToken] = useState([]);
- 
- useEffect(()=>{
-    const storage = window.localStorage.getItem('token');
+
+  useEffect(() => {
+    const storage = window.localStorage.getItem("token");
     setToken(storage);
   }, []);
 
-const logUser = (e) =>{
+  const logUser = (e) => {
     e.preventDefault();
     const user = {
-        email: e.target.mail.value,
-        password: e.target.password.value
-    }
-    
+      email: e.target.mail.value,
+      password: e.target.password.value,
+    };
+
     axios({
-        method: "POST",
-        url: "http://localhost:5000/api/auth/login",      
-        data: user,
+      method: "POST",
+      url: "http://localhost:5000/api/auth/login",
+      data: user,
     })
-    .then((response) => {
-      if(response.data.success === true) window.location.href = "http://localhost:3002/";
-      setUser(response.data)
-      window.localStorage.setItem('token', response.data.token.split(' ')[1]);
-      console.log(response.data.token.split(' ')[1]);
-    })
-    .catch((error) => error);
+      .then((response) => {
+        if (response.data.success === true)
+          window.location.href = "http://localhost:3002/";
+        setUser(response.data);
+        window.localStorage.setItem("token", response.data.token.split(" ")[1]);
+        console.log(response.data.token.split(" ")[1]);
+      })
+      .catch((error) => error);
     e.target.reset();
-}
-  console.log();
+  };
 
   return (
-    <AppContext.Provider value={{ Jakaps, discussion, user, logUser, setDiscussion }}>
-      <div className="container"> 
-        {!token ? <Welcome/> :
-        <>
-        <Menu />
-        <Routes>
-        
-        <Route path="/signup" element={<SignUp/>}/>
-          <Route exact path="/" element={<Home />} />
-          
-        </Routes>
-        </>
-        }
+    <AppContext.Provider
+      value={{ Jakaps, discussion, user, logUser, setDiscussion }}
+    >
+      <div className="container">
+        {!token ? (
+          <Welcome />
+        ) : (
+          <>
+            <Menu />
+            <Routes>
+              <Route path="/signup" element={<SignUp />} />
+              <Route exact path="/" element={<Home />} />
+            </Routes>
+          </>
+        )}
       </div>
     </AppContext.Provider>
   );
